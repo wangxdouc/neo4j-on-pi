92eced982933e4fef30e146e929b06e674df3f53
wangxdouc <wangxdouc@gmail.com>
2017-07-11 22:40:14 +0800

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Try to make a out-of-box script to build Neo4j database on Raspberry Pi 2 Model B.
+
+# Install JDK environment
+
+
+# Install Neo4j
+
+
+# Configure Neo4j
+
+
+# Start Neo4j
